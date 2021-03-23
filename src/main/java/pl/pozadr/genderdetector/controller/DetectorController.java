e9762cba53d387dfc4ac2e0af9b5bcccc71c4093
Pozadr <adpozor@gmail.com>
2021-03-23 11:24:03 +0100

    dev -> DetectorController -> getTokens and parameters exceptions

--- src/main/java/pl/pozadr/genderdetector/controller/DetectorController.java
+++ src/main/java/pl/pozadr/genderdetector/controller/DetectorController.java
@@ -3,12 +3,16 @@ package pl.pozadr.genderdetector.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pozadr.genderdetector.exceptions.PageSizeParameterException;
 import pl.pozadr.genderdetector.service.DetectorService;
 import pl.pozadr.genderdetector.validators.ControllerParametersValidator;
+
+import java.util.List;
 
 
 @RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE}, path = "/gender-detector")
@@ -33,6 +37,12 @@ public class DetectorController {
                 : detectorService.checkAllTokensInName(name);
 
         return ResponseEntity.ok(gender);
+    }
+
+    @GetMapping("/v1/tokens")
+    public ResponseEntity<List<String>> getTokens(@RequestParam int pageNo, @RequestParam int pageSize) {
+        ControllerParametersValidator.validateGetTokensParams(pageNo, pageSize);
+        return ResponseEntity.ok(detectorService.getTokens(pageNo, pageSize));
     }
 
 }
--- src/main/java/pl/pozadr/genderdetector/validators/ControllerParametersValidator.java
+++ src/main/java/pl/pozadr/genderdetector/validators/ControllerParametersValidator.java
@@ -1,18 +1,45 @@
 package pl.pozadr.genderdetector.validators;
 
+import pl.pozadr.genderdetector.config.AppConstants;
 import pl.pozadr.genderdetector.exceptions.MethodParameterException;
 import pl.pozadr.genderdetector.exceptions.NameParameterException;
+import pl.pozadr.genderdetector.exceptions.PageNumberParameterException;
+import pl.pozadr.genderdetector.exceptions.PageSizeParameterException;
 
 public final class ControllerParametersValidator {
     private ControllerParametersValidator() {
+    }
+
+
+    public static boolean validateGetTokensParams(int pageNo, int pageSize) {
+
+        return isPageNoValid(pageNo) && isPageSizeValid(pageSize);
+    }
+
+    public static boolean validateGetGenderParams(String name, String method) {
+        return isNameParamValid(name) && isMethodParamValid(method);
     }
 
     public static boolean isMethodFirstToken(String method) {
         return method.equalsIgnoreCase(CheckMethod.FIRST_TOKEN.toString());
     }
 
-    public static boolean validateGetGenderParams(String name, String method) {
-        return isNameParamValid(name) && isMethodParamValid(method);
+
+    private static boolean isPageSizeValid(int pageSize) {
+        if (pageSize > AppConstants.PAGE_SIZE_LIMIT
+                || pageSize < 0) {
+            throw new PageSizeParameterException("Given pageSize is out of range. Minimum = 0 , maximum = "
+                    + AppConstants.PAGE_SIZE_LIMIT);
+        }
+        return true;
+    }
+
+    private static boolean isPageNoValid(int pageNo) {
+        if (pageNo < 0) {
+            throw new PageNumberParameterException("Given pageNo is out of range. Minimum = 0 , maximum =  "
+                    + Integer.MAX_VALUE);
+        }
+        return true;
     }
 
     private static boolean isNameParamValid(String name) {
